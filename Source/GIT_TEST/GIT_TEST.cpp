2e77a0244b2a2158f7e3a8b314e2c86f7c5a1fa6
YoungHun Jeong <jyh7a@naver.com>
2026-07-01 09:23:18 +0900

    [ADD] Initialize Project

---
+++ Source/GIT_TEST/GIT_TEST.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GIT_TEST.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GIT_TEST, "GIT_TEST" );
